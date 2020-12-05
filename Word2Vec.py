e12879be7cb9051157f0343b1f48212ce345d8a0
Ivan Sinaga <ivansinaga372@gmail.com>
2020-12-05 18:43:04 +0700

    edit:word2vec

--- Word2Vec.py
+++ Word2Vec.py
@@ -29,30 +29,30 @@ for i in sent_tokenize(f):
     data.append(temp) 
 
 
-print(data)
+# print(data)
   
-# # Create CBOW model 
-# model1 = gensim.models.Word2Vec(data, min_count = 1,  
-#                               size = 100, window = 5) 
+# Create CBOW model 
+model1 = gensim.models.Word2Vec(data, min_count = 1,  
+                              size = 100, window = 5) 
   
-# # Print results 
-# print("Cosine similarity between 'alice' " + 
-#                "and 'wonderland' - CBOW : ", 
-#     model1.similarity('alice', 'wonderland')) 
+# Print results 
+print("Cosine similarity between 'alice' " + 
+               "and 'wonderland' - CBOW : ", 
+    model1.similarity('alice', 'wonderland')) 
       
-# print("Cosine similarity between 'alice' " +
-#                  "and 'found' - CBOW : ", 
-#       model1.similarity('alice', 'found')) 
+print("Cosine similarity between 'alice' " +
+                 "and 'found' - CBOW : ", 
+      model1.similarity('alice', 'found')) 
   
-# # Create Skip Gram model 
-# model2 = gensim.models.Word2Vec(data, min_count = 1, size = 100, 
-#                                              window = 5, sg = 1) 
+# Create Skip Gram model 
+model2 = gensim.models.Word2Vec(data, min_count = 1, size = 100, 
+                                             window = 5, sg = 1) 
   
-# # Print results 
-# print("Cosine similarity between 'alice' " +
-#           "and 'wonderland' - Skip Gram : ", 
-#     model2.similarity('alice', 'wonderland')) 
+# Print results 
+print("Cosine similarity between 'alice' " +
+          "and 'wonderland' - Skip Gram : ", 
+    model2.similarity('alice', 'wonderland')) 
       
-# print("Cosine similarity between 'alice' " +
-#             "and 'found' - Skip Gram : ", 
-#       model2.similarity('alice', 'found')) 
+print("Cosine similarity between 'alice' " +
+            "and 'found' - Skip Gram : ", 
+      model2.similarity('alice', 'found')) 
